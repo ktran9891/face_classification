e3f5336ce789714bfa02225f9774bcd19abbdfad
Kevin Tran <ktrain9891@gmail.com>
2019-04-03 13:23:07 -0400

    Update inference function for new Keras

--- face_classification/utils/inference.py
+++ face_classification/utils/inference.py
@@ -4,7 +4,12 @@ import numpy as np
 from keras.preprocessing import image
 
 def load_image(image_path, grayscale=False, target_size=None):
-    pil_image = image.load_img(image_path, grayscale, target_size)
+    color_mode = 'grayscale'
+    if grayscale == False:
+        color_mode = 'rgb'
+    else:
+        grayscale = False
+    pil_image = image.load_img(image_path, grayscale, color_mode, target_size)
     return image.img_to_array(pil_image)
 
 def load_detection_model(model_path):
